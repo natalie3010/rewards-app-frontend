c836bc517ee8a1741d8beaf67fc0a7bd40acbe61
tasha.jassal@capgemini.com-at-285942147264 <tasha.jassal@capgemini.com>
2022-05-18 10:18:13 +0100

    Login Page completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import FormLayoutNominator from "./pages/FormLayoutNominator";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FormLayoutNominee from "./pages/FormLayoutNominee";
 import SideBar from "./components/Sidebar";
-import Login from "./pages/Admin";
+import Login from "./pages/Admin/Login";
 
 function App() {
   return (
