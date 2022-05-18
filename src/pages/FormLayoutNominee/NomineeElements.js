df390f9f1289c06a7a91d7164d30c1103a8662d6
Marie-Jubilee <marie-jubilee.muntu-nkaye@capengadm.onmicrosoft.com>
2022-05-18 14:07:30 +0100

    "i" modal

--- src/pages/FormLayoutNominee/NomineeElements.js
+++ src/pages/FormLayoutNominee/NomineeElements.js
@@ -1,19 +1,15 @@
 import styled from "styled-components";
 
 export const TitleContainer = styled.div`
-margin: 4% ;
-top: 5%;
-
+  margin: 4%;
+  top: 5%;
 `;
 
-
 export const Title = styled.h1`
-color: #0070ad !important;
-font-size: 1.8rem;
-font-family: 'Ubuntu';
-text-align: center;
-
-
+  color: #0070ad !important;
+  font-size: 1.8rem;
+  font-family: "Ubuntu";
+  text-align: center;
 `;
 
 export const FormContainer = styled.div`
@@ -21,5 +17,9 @@ export const FormContainer = styled.div`
   width: 50%;
   position: absolute;
   margin-left: 25%;
-  
+`;
+
+export const IconImg = styled.img`
+  width: 16px;
+  height: 16spx;
 `;
