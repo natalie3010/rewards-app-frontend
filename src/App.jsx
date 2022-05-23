2f9e7d0da6e1a86338cf0c0082577b419f7486f7
EKcap <98160631+EKcap@users.noreply.github.com>
2022-05-23 14:54:07 +0100

    Added landing page and animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import "./App.css";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { PublicClientApplication } from "@azure/msal-browser";
-import { msalConfig } from "./pages/Authentication/SPA/authConfig";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./pages/Authentication/SPA/styles/index.css";
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { PublicClientApplication } from '@azure/msal-browser';
+import { msalConfig } from './pages/Authentication/SPA/authConfig';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './pages/Authentication/SPA/styles/index.css';
 
-import "bootstrap/dist/css/bootstrap.css";
-import Authentication from "./pages/Authentication/SPA/Authentication";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FormLayoutNominee from "./pages/FormLayoutNominee";
-import SideBar from "./components/Sidebar";
-import Login from "./pages/Admin/Login";
-import Filter from "./pages/Admin/Filter";
+import 'bootstrap/dist/css/bootstrap.css';
+import Authentication from './pages/Authentication/SPA/Authentication';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FormLayoutNominee from './pages/FormLayoutNominee';
+import SideBar from './components/Sidebar';
+import Login from './pages/Admin/Login';
+import Filter from './pages/Admin/Filter';
+import Landing from './pages/Landing';
 
 export const msalInstance = new PublicClientApplication(msalConfig);
 
@@ -20,14 +21,17 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route
-          path="/"
+          path="/auth"
           element={<Authentication msalInstance={msalInstance} />}
         />
         <Route path="/login" element={<Login />} />
         <Route path="/filter" element={<Filter />} />
 
-        <Route path="/nominee" element={
+        <Route
+          path="/nominee"
+          element={
             <>
               <FormLayoutNominee />
             </>
